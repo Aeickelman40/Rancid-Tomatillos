162c6d9e329cece6c676578dcac438247d102320
Joe Haefling <josephhaefling@gmail.com>
2020-07-04 10:55:00 -0600

    Add tests for ErrorPage and Movies components

--- src/Movies/Movies.test.js
+++ src/Movies/Movies.test.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, getByTestId} from '@testing-library/react';
+import '@testing-library/jest-dom'
 import Movies from './Movies';
 
-test('renders learn react link', () => {
-    const {
-        getByText
-    } = render( < Movies / > );
-    const linkElement = getByText(/Hello Guest/i);
-    expect(linkElement).toBeInTheDocument();
-});
+describe ('Movies', () => {
+    let movies = null
+    
+    beforeEach(() => {
+        movies = {
+            movies: [
+            {
+                id: 1,
+                poster_path: 'examplePosterPath1',
+                backdrop_path: 'exampleBackdropPath1',
+                title: 'exampleTitle1',
+                average_rating: 1,
+                release_date: '2001-01-01'
+            }, {
+                id: 2,
+                poster_path: 'examplePosterPath2',
+                backdrop_path: 'exampleBackdropPath2',
+                title: 'exampleTitle2',
+                average_rating: 2,
+                release_date: '2002-02-02'
+            }
+            ]
+        }
+    })
+
+    it('should be true', () => {
+        expect(true).toEqual(true)
+    })
+
+    it('should have movies data', () => {
+        const { getByTestId } = render(<Movies moviesData={ movies } />)
+        const movie = getByTestId('all-movies');
+        expect(movie).toBeInTheDocument()
+    })
+})
