b7a287fb80e9193b99b141551248b054a0420f8a
Joe Haefling <josephhaefling@gmail.com>
2020-07-09 16:48:05 -0600

    Render movie rating to the home page

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -5,13 +5,19 @@ function Movie(props) {
     
     const {id, backdrop_path, release_date, average_rating, title} = props.movie
     const { onClick, userRatings } = props
-    if (userRatings)
+    let movieRating = "You haven't rated this movie yet."
     
     // const movieRating = userRatings.find(rating => rating.movie_id === id)
     
     if (props.appState.isLoggedIn) {
-        const userRatings = props.appState.userInfo.userRating || "You haven't rated this movie"
+        if (userRatings) {
+            userRatings.find(rating => {
+               rating.movie_id === id ? movieRating =rating.rating : movieRating = "You haven't rated this movie yet."
+            })   
+        }
     }    
+    console.log(movieRating);
+    
     return (
             <NavLink to={`${/movie/}${id}`}>
               <button name='movieId' value={ id } className = 'movie-card' onClick={ (event) => onClick(event) }>
@@ -21,6 +27,7 @@ function Movie(props) {
                  backdrop-image= { backdrop_path }
                  release-date= { release_date }
                  average-rating= { average_rating }
+                 user-rating= { movieRating }
                  title={ title }
                  data-testid='background'
                     style={ {
@@ -33,7 +40,7 @@ function Movie(props) {
                             <p className='title'>{title}</p>
                             <p>Release Date:{ release_date }</p>
                             <p>Average Rating:{ Math.round(average_rating) }</p>
-                            {/* <p>Your Rating:{ userRatings }</p> */}
+                            <p>Your Rating:{ movieRating }</p>
                  </section>
                 </button>
             </NavLink>
